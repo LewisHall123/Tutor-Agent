1d5847c98b6d06ec67d420123961120e8366d961
LewisHall123 <156428192+LewisHall123@users.noreply.github.com>
2025-10-22 09:12:49 +0100

    Config file containing file paths to where .txt files are stored locally

---
+++ config.py
@@ -0,0 +1,5 @@
+# File paths to where notes and embeddings are saved
+NOTES_DIR = "/Users/lewishall/Desktop/AI_Projects/AI Tutor/txt_python/*.txt"
+INDEX_PATH = "/Users/lewishall/Desktop/AI_Projects/AI Tutor/data/embeddings_faiss"
+TOPICS_DIR = "/Users/lewishall/Desktop/AI_Projects/AI Tutor/txt_python_meta/1.1.topics.txt"
+PROGRESS_DIR = "/Users/lewishall/Desktop/AI_Projects/AI Tutor/txt_python_meta/1.1.progress.txt"
